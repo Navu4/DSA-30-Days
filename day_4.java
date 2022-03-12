02ebf837a9002ecac56c9ff2f5cfcd24ac4877b0
Navu4 <snav.jot5454@gmail.com>
2022-03-13 00:46:06 +0530

    fourth day compeleted

--- day_4.java
+++ day_4.java
@@ -38,10 +38,111 @@ public class day_4 {
         6. K Transaction Allowed 
     */
 
+    // 1. Buy And Sell Stocks - One Transaction Allowed
+    public static int oneTransactionAllowed(int[] arr, int n) {
+        int leastPrice = (int)1e9;
+        int maxProfit = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < leastPrice){
+                leastPrice = arr[i];
+            }
+
+            int todaysProfit = arr[i] - leastPrice;
+            if(todaysProfit > maxProfit){
+                maxProfit = todaysProfit;
+            }
+        }
+        return maxProfit;
+    }
+
+    // 2. Infinite Transaction Allowed
+    public static int infiniteTransactionAllowed_myAns(int[] arr, int n) {
+        int leastPrice = (int)1e9;
+        int currentProfit = 0;
+        int totalProfit = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < leastPrice){
+                leastPrice = arr[i];
+            }
+
+            int todaysProfit = arr[i] - leastPrice;
+            if(currentProfit > todaysProfit){
+                totalProfit += currentProfit;
+                currentProfit = 0;
+                leastPrice = arr[i];
+            } else {
+                currentProfit = todaysProfit;
+            }
+            
+        }
+
+
+        return totalProfit;
+    }
+
+    public static int infiniteTransactionAllowed_sirAns(int[] arr, int n) {
+        int bd = 0, sd = 0, profit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+           if(arr[i] >= arr[i - 1]){
+            sd++;
+           } else {
+            profit += arr[sd] - arr[bd];
+            bd = sd = i;
+           }
+        }
+        profit += arr[sd] - arr[bd];
+        return profit;
+    }
+
+    //   3. With Transaction Fee and Infinite Transaction Allowed
+    public static int withTransactionFeeInfiniteTransaction(int[] arr, int n) {
+        return 0;
+    }
+
+    //4. With Cool Down and Infinite Transaction Allowed
+    public static int withCoolDownFeeInfiniteTransaction(int[] arr, int n) {
+        return 0;
+    }
+
+    // 5. Two Transaction Allowed
+    public static int twoTransactionAllowed(int[] arr, int n) {
+        return 0;
+    }
+
+    // 6. K Transaction Allowed 
+    public static int kTransactionAllowed(int[] arr, int n) {
+        return 0;
+    }
+
+
+    public static void buyAndSell() {
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        // System.out.println(oneTransactionAllowed(arr, n));
+        System.out.println(infiniteTransactionAllowed(arr, n));
+    }
+
+    public static void buyAndSellWithFee() {
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int fee = scn.nextInt();
+    }
 
 
     public static void main(String[] args) {
-        
+        // buyAndSell();
     }
 
 }
